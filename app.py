4bc77e7b71ced09b39403f034b251b07be8ac165
Tosin Akinosho <takinosh@redhat.com>
2023-09-27 09:48:04 -0400

    updating code

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
 import requests
+import os
+
+SERVICENOW_INSTANCE_URL = os.getenv("SERVICENOW_INSTANCE_URL")
+SERVICE_NOW_USERNAME = os.getenv("SERVICE_NOW_USERNAME")
+SERVICE_NOW_PASSWORD = os.getenv("SERVICE_NOW_PASSWORD")
+
 
 app = Flask(__name__)
 
-SERVICE_NOW_USERNAME = "your_username"
-SERVICE_NOW_PASSWORD = "your_password"
 
 @app.route('/alerts', methods=['POST'])
 def alerts():
@@ -14,7 +18,7 @@ def alerts():
     return "OK", 200
 
 def create_service_now_incident(alert):
-    servicenow_url = "https://YOUR_SERVICENOW_INSTANCE_URL/api/now/table/incident"
+    servicenow_url = "https://"{SERVICENOW_INSTANCE_URL}"/api/now/table/incident"
     headers = {
         "Content-Type": "application/json",
     }
